4d8717c83a500c8fca49c25b1a6e6784691db028
Levi Wong <lei.huang@binance.com>
2020-09-27 14:03:18 +0800

    chore: tail call optimization

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-export default function isCyclic(object, seen = new WeakSet().add(object)) {
-	const props = getProps(object);
-	for (const prop of props) {
-		const value = object[prop];
-		if (value instanceof Object) {
-			return seen.has(value) || isCyclic(value, seen.add(value));
-		}
-	}
+export default function isCyclic (object, seen = new WeakSet().add(object)) {
+  const props = getProps(object)
+  let len = props.length
 
-	return false;
+  while (len--) {
+    const value = object[props[len]]
+    if (value instanceof Object) {
+      if (seen.has(value)) {
+        return true
+      }
+      return isCyclic(value, seen.add(value))
+    }
+  }
+
+  return false
 }
 
-function getProps(object) {
-	const props = Object.keys(object);
-	if (Object.getOwnPropertySymbols) {
-		const symbolProperties = Object.getOwnPropertySymbols(object);
-		Array.prototype.push.apply(props, symbolProperties);
-	}
+function getProps (object) {
+  const props = Object.keys(object)
+  if (Object.getOwnPropertySymbols) {
+    const symbolProperties = Object.getOwnPropertySymbols(object)
+    Array.prototype.push.apply(props, symbolProperties)
+  }
 
-	return props;
+  return props
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,23 +1,23 @@
-import test from 'ava';
-import isCyclic from '.';
+import test from 'ava'
+import isCyclic from '.'
 
-test('detects regular cyclic object', t => {
-	const object = {a: 1, b: 2, c: {d: 9}};
-	object.c.e = object;
+test('detects regular cyclic object', (t) => {
+  const object = { a: 1, b: 2, c: { d: 9, e: { f: 8 } } }
+  object.c.e.g = object
 
-	t.is(isCyclic(object), true);
-});
+  t.is(isCyclic(object), true)
+})
 
-test('detects cyclic object with symbol keys', t => {
-	const object = {a: 1, b: 2};
-	const key = Symbol('key');
-	object[key] = object;
+test('detects cyclic object with symbol keys', (t) => {
+  const object = { a: 1, b: 2 }
+  const key = Symbol('key')
+  object[key] = object
 
-	t.is(isCyclic(object), true);
-});
+  t.is(isCyclic(object), true)
+})
 
-test('recognizes non-cyclic object', t => {
-	const object = {a: 1, b: 2, c: {d: 8, e: {f: 9}}};
+test('recognizes non-cyclic object', (t) => {
+  const object = { a: 1, b: 2, c: { d: 8, e: { f: 9 } } }
 
-	t.is(isCyclic(object), false);
-});
+  t.is(isCyclic(object), false)
+})
